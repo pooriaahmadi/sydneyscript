6c8029579c39ebca8c2259257f05753f00a0a000
pooriaahmadi <pooriababak444@gmail.com>
2022-06-09 21:07:50 -0400

    first commit

---
+++ shell.py
@@ -0,0 +1,15 @@
+import sydneyscript
+
+while True:
+    text = input('SydneyScript > ')
+    if text.strip() == "":
+        continue
+    result, error = sydneyscript.run('<stdin>', text)
+
+    if error:
+        print(error.as_string())
+    elif result:
+        if len(result.elements) == 1:
+            print(repr(result.elements[0]))
+        else:
+            print(repr(result))
